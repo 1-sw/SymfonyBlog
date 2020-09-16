3a34455e9780631a4bbbc93920dc703f412a8fd3
1-sw <63602047+1-sw@users.noreply.github.com>
2020-09-16 18:40:39 +0300

    Controller syntax fix

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -3,13 +3,22 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomePageController extends AbstractController {
+class HomePageController extends AbstractController 
+{
     /**
      * @Route("/", name="home_page")
      */
-    public function index() {
-        return $this->render('home_page/index.html.twig',['controller_name' => 'HomePageController',]);
+    public function index() 
+	{
+        return $this->render
+		(
+			'home_page/index.html.twig',
+			[
+				'controller_name' => 'HomePageController'
+			]
+		);
     }
 }
