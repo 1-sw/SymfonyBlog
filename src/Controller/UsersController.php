d5d5cc87e7c30770729b2d13c30495992ed09af9
1-sw <63602047+1-sw@users.noreply.github.com>
2020-09-21 14:39:12 +0300

    Configure Users table

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
 
 use App\Entity\Users;
+use App\Entity\Post;
 use App\Repository\UsersRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,19 +14,45 @@ use Symfony\Component\Routing\Annotation\Route;
 /**
  * @Route("/users")
  */
-class PostController extends AbstractController
+class UsersController extends AbstractController
 {
     private $usersRepository;
-
-    public function __construct(PostRepository $usersRepository)
+	private $users;
+	private $postApi;
+	
+    public function __construct(UsersRepository $usersRepository)
     {
         $this->usersRepository = $usersRepository;
     }
+	/**
+	 * @Route("/new")
+	 */
+	public function newUser(): Response
+	{
+		$this->users = new Users();
+		$this->postApi = new Post();
+		
+		$this->users
+		    ->setName(str_repeat('UserName', 1))
+            ->setPassword(str_repeat('asd123', 2))
+            ->setPostId(random_int(1, $this->postApi->getId))
+			->setCreatedAt(new \DateTime())
+			->setRole("ROLE_USER");
+			
+		$entityManager->persist($this->users);
+		$entityManager->flush();
+		
+		return new Response($this->users->getId);
+	}
 
-    public function posts(): Response
+
+	/**
+	 * @Route("")
+     */
+    public function users(): Response
     {
-        $users = $this->usersRepository->findAll();
-        dd($users);
+        $this->users = $this->usersRepository->findAll();
+        dd($this->users);
         return new Response();
     }
 }
