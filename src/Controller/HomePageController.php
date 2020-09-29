ca8db9c2f19bad21ff7ce50b708cecde45abec74
1-sw <cppa619@gmail.com>
2020-09-29 21:21:07 +0300

    Add some funny comments, like this: {# ATTENTION!!! COPYPASTE HERE (C)symfony docs#} and fixx syntax

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -4,11 +4,13 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomePageController extends AbstractController  {
-    /**
-     * @Route("/home", name="home_page")
-     */
-    public function index() {
-        return $this->render('home_page\index.html.twig',['controller_name' => 'HomePageController']);
-    }
+class HomePageController extends AbstractController  
+{
+	/**
+     	* @Route("/home", name="home_page")
+     	*/
+    	public function index() 
+	{
+        	return $this->render('home_page\index.html.twig',['controller_name' => 'HomePageController']);
+    	}
 }
