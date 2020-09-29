815760365c3a19e104c0c81b092a5755770dc6fb
1-sw <cppa619@gmail.com>
2020-09-29 17:59:12 +0300

    Edit syntax in PostManager. Trying to udnerstand whats going on

--- src/Manager/PostManager.php
+++ src/Manager/PostManager.php
@@ -1,6 +1,5 @@
 <?php
 
-declare(strict_types=1);
 
 namespace App\Manager;
 
@@ -11,25 +10,23 @@ use Doctrine\ORM\EntityManagerInterface;
 
 class PostManager
 {
-    /** @var EntityManagerInterface */
-    private $em;
+	/** @var EntityManagerInterface */
+    	private $em;
 
-    /** @var PostDataTransformer */
-    private $dataTransformer;
+    	/** @var PostDataTransformer */
+    	private $dataTransformer;
 
-    public function __construct(EntityManagerInterface $em, PostDataTransformer $dataTransformer)
-    {
-        $this->em = $em;
-        $this->dataTransformer = $dataTransformer;
-    }
+    	public function __construct(EntityManagerInterface $em, PostDataTransformer $dataTransformer)
+    	{
+        	$this->em = $em;
+        	$this->dataTransformer = $dataTransformer;
+    	}
 
-    public function create(PostRequest $request): PostResponse
-    {
-        $post = $this->dataTransformer->transform($request);
-
-        $this->em->persist($post);
-        $this->em->flush();
-
-        return $this->dataTransformer->reverseTransform($post);
-    }
+    	public function create(PostRequest $request): PostResponse
+    	{
+        	$post = $this->dataTransformer->transform($request);
+        	$this->em->persist($post);
+        	$this->em->flush();
+        	return $this->dataTransformer->reverseTransform($post);
+    	}
 }
