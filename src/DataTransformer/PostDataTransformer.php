fa20820714875ba0ffead576cc10ca6652da34d0
1-sw <cppa619@gmail.com>
2020-09-29 17:55:21 +0300

    Edit syntax in DataTransformer. Trying to udnerstand whats going on

--- src/DataTransformer/PostDataTransformer.php
+++ src/DataTransformer/PostDataTransformer.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\DataTransformer;
 
 use App\Entity\Post;
@@ -10,22 +8,22 @@ use App\Model\Response\PostResponse;
 
 class PostDataTransformer
 {
-    public function transform(PostRequest $postRequest): Post
-    {
-        return (new Post())
-            ->setTitle($postRequest->getTitle())
-            ->setContent($postRequest->getContent())
-            ->setCreatedAt(new \DateTime())
-            ->setUpdatedAt(new \DateTime());
-    }
+	public function transform(PostRequest $postRequest): Post
+    	{
+        	return (new Post())
+            		->setTitle($postRequest->getTitle())
+            		->setContent($postRequest->getContent())
+            		->setCreatedAt(new \DateTime())
+            		->setUpdatedAt(new \DateTime());
+    	}
 
-    public function reverseTransform(Post $post): PostResponse
-    {
-        return (new PostResponse())
-            ->setId($post->getId())
-            ->setContent($post->getContent())
-            ->setTitle($post->getTitle())
-            ->setCreatedAt($post->getCreatedAt()->format('Y-m-d H:i:s'))
-            ->setUpdatedAt($post->getUpdatedAt()->format('Y-m-d H:i:s'));
-    }
+    	public function reverseTransform(Post $post): PostResponse
+    	{
+        	return (new PostResponse())
+            		->setId($post->getId())
+            		->setContent($post->getContent())
+            		->setTitle($post->getTitle())
+            		->setCreatedAt($post->getCreatedAt()->format('Y-m-d H:i:s'))
+            		->setUpdatedAt($post->getUpdatedAt()->format('Y-m-d H:i:s'));
+    	}
 }
