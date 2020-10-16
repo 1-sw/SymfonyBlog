42f969ca35a028077d6c953a47fffee969f8fd09
1-sw <cppa619@gmail.com>
2020-10-16 21:03:25 +0300

    Fix error with namespace

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,14 +3,19 @@
 namespace App\Controller;
 
 use App\Manager\UserManager;
+
 use App\Model\Request\UserRequest;
 use App\Repository\UserRepository;
+
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
