e20a2e72fd8a40cc75bd56d0800a4660bfd9040f
1-sw <cppa619@gmail.com>
2020-10-16 19:02:13 +0300

    Post table passed test with success. I add some comments at the begining of file

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -1,8 +1,18 @@
 <?php
 
-//declare(strict_types=1);
+// Current version of file of project works nice
+// Now you can:
+// [1] Send Post/Get requests
+// [2] Work with maded DB 
+// Future version will:
+// [1] With ParamConverter
+// [2] With Strict type syntax
+// [3] With php7.4 typehint
+
+declare(strict_types=1);
 
 namespace App\Controller;
+
 use App\Manager\PostManager;
 use App\Model\Request\PostRequest;
 use App\Repository\PostRepository;
@@ -18,35 +28,35 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 /**
  * @Route("/posts")
  */
-class PostController extends AbstractController 
+class PostController extends AbstractController
 {
 	private $postRepository;
 
-    	public function __construct(PostRepository $postRepository) 
-    	{
-        	$this->postRepository = $postRepository;
-    	}
-    	/**
-     	 * @Route("", methods={"POST"})
-     	 */
-    	public function create(Request $request,PostManager $postManager,SerializerInterface $serializer, ValidatorInterface $validator): JsonResponse 
+	public function __construct(PostRepository $postRepository)
+	{
+		$this->postRepository = $postRepository;
+	}
+	/**
+	 * @Route("", methods={"POST"})
+	 */
+	public function create(Request $request, PostManager $postManager, SerializerInterface $serializer, ValidatorInterface $validator): JsonResponse
 	{
 		$json = $request->getContent();
-	    	$body = json_decode($json, true);
-	    	$postRequest = (new PostRequest())->setContent($body['content'])->setTitle($body['title']);
-	    	$postRequest = $serializer->deserialize($json, PostRequest::class, 'json');
-	    	$validator->validate($postRequest);
-            	$postResponse = $postManager->create($postRequest);
-	    	return new JsonResponse($postResponse, Response::HTTP_CREATED);
-    	}
+		$body = json_decode($json, true);
+		$postRequest = (new PostRequest())->setContent($body['content'])->setTitle($body['title']);
+		$postRequest = $serializer->deserialize($json, PostRequest::class, 'json');
+		$validator->validate($postRequest);
+		$postResponse = $postManager->create($postRequest);
+		return new JsonResponse($postResponse, Response::HTTP_CREATED);
+	}
 
-    	/**
-     	 * @Route("", methods={"GET"})
-     	 */
-    	public function posts(): Response
-    	{
-        	$posts = $this->postRepository->findAll();
-        	dd($posts);
-        	return new Response();
-    	}
+	/**
+	 * @Route("", methods={"GET"})
+	 */
+	public function posts(): Response
+	{
+		$posts = $this->postRepository->findAll();
+		dd($posts);
+		return new Response();
+	}
 }
